fb35655ccf556ec9777f51e25f2a895a7635be22
Cameron Hudson <CameronHudson8@gmail.com>
2021-01-16 13:17:01 -0800

    feat: add CharacterService with basic creation function

---
+++ src/app/services/character/character.service.spec.ts
@@ -0,0 +1,55 @@
+import { HttpResponse } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { AuthenticatorService } from '../authenticator/authenticator.service';
+
+import { CharacterService } from './character.service';
+
+describe('CharacterService', () => {
+
+  let service: CharacterService;
+  let mockAuthenticatorService: jasmine.SpyObj<AuthenticatorService>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: AuthenticatorService,
+          useValue: jasmine.createSpyObj('AuthenticatorService', ['requestWithAuth'])
+        }
+      ]
+    });
+
+    mockAuthenticatorService = TestBed.inject(AuthenticatorService) as jasmine.SpyObj<AuthenticatorService>;
+    service = TestBed.inject(CharacterService);
+
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should properly fetch basic character data from token', async () => {
+
+    // Arrange
+    const tokenData = {
+      "CharacterID": 95465499,
+      "CharacterName": "CCP Bartender",
+      "ExpiresOn": "2017-07-05T14:34:16.5857101",
+      "Scopes": "esi-characters.read_standings.v1",
+      "TokenType": "Character",
+      "CharacterOwnerHash": "lots_of_letters_and_numbers",
+      "IntellectualProperty": "EVE"
+    };
+    mockAuthenticatorService.requestWithAuth
+      .and.resolveTo(new HttpResponse<any>({ status: 200, body: tokenData }));
+
+    // Act
+    const character = await service.getCharacter();
+
+    // Assert
+    expect(character.id).toBe(95465499);
+    expect(character.name).toBe('CCP Bartender');
+
+  });
+
+});
---
+++ src/app/services/character/character.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Character } from 'src/app/entities/Character';
+import { AuthenticatorService } from '../authenticator/authenticator.service';
+
+@Injectable({ providedIn: 'root' })
+export class CharacterService {
+
+  constructor(
+    private authenticatorService: AuthenticatorService
+  ) { }
+
+  async getCharacter(): Promise<Character> {
+
+    type CharacterDataBasic = {
+      CharacterID: number;
+      CharacterName: string;
+      ExpiresOn: string;
+      Scopes: string;
+      TokenType: string;
+      CharacterOwnerHash: string;
+      IntellectualProperty: string;
+    };
+
+    const response = await this.authenticatorService.requestWithAuth<CharacterDataBasic>(
+      'get',
+      'https://login.eveonline.com/oauth/verify'
+    );
+    const characterData = response.body;
+    const character = new Character(
+      this.authenticatorService
+    );
+    character.id = characterData.CharacterID;
+    character.name = characterData.CharacterName;
+    return character;
+
+  }
+
+}
