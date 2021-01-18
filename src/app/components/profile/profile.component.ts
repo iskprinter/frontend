fb0b2a23ffe9b6032415fb56807d23279f81a62f
Cameron Hudson <CameronHudson8@gmail.com>
2021-01-17 23:58:44 -0800

    refactor: improve typing

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,6 +1,5 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { AuthenticatorService } from 'src/app/services/authenticator/authenticator.service';
 import { Character, CharacterLocation } from 'src/app/entities/Character';
 import { CharacterService } from 'src/app/services/character/character.service';
 
@@ -17,27 +16,23 @@ export class ProfileComponent implements OnInit {
   characterWalletBalance: number;
 
   constructor(
-    public authenticatorService: AuthenticatorService,
     public characterService: CharacterService,
   ) { }
 
   async ngOnInit(): Promise<void> {
-    if (this.authenticatorService.isLoggedIn()) {
-
-      this.character = await this.characterService.getCharacterFromToken();
-      const [
-        characterLocation,
-        characterPortrait,
-        characterWalletBalance
-      ] = await Promise.allSettled([
-        this.characterService.getLocationOfCharacter(this.character),
-        this.characterService.getPortraitOfCharacter(this.character),
-        this.characterService.getWalletBalanceOfCharacter(this.character)
-      ]);
-      this.characterLocation = characterLocation.status === 'fulfilled' ? characterLocation.value : undefined;
-      this.characterPortrait = characterPortrait.status === 'fulfilled' ? characterPortrait.value : undefined;
-      this.characterWalletBalance = characterWalletBalance.status === 'fulfilled' ? characterWalletBalance.value : undefined;
-    }
+    this.character = await this.characterService.getCharacterFromToken();
+    const [
+      characterLocation,
+      characterPortrait,
+      characterWalletBalance
+    ] = await Promise.allSettled([
+      this.characterService.getLocationOfCharacter(this.character),
+      this.characterService.getPortraitOfCharacter(this.character),
+      this.characterService.getWalletBalanceOfCharacter(this.character)
+    ]);
+    this.characterLocation = characterLocation.status === 'fulfilled' ? characterLocation.value : undefined;
+    this.characterPortrait = characterPortrait.status === 'fulfilled' ? characterPortrait.value : undefined;
+    this.characterWalletBalance = characterWalletBalance.status === 'fulfilled' ? characterWalletBalance.value : undefined;
   }
 
 }
